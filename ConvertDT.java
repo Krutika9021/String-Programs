10c5fb70a28790c0b4b4e58919edc5ae29f2c952
Krutika9021 <109949451+Krutika9021@users.noreply.github.com>
2022-08-08 01:18:24 -0800

    Add files via upload

---
+++ ConvertDT.java
@@ -0,0 +1,12 @@
+// How do you convert String to an integer?
+
+public class ConvertDT
+{
+     public static void main(String[] args) 
+     {
+
+      String number = "1";
+      int result = Integer.parseInt(number);
+      System.out.println(result);
+  }
+}
